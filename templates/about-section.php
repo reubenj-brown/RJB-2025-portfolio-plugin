e07a385a6d1a801bdb7c7331929df62d883ba298
reubenj-brown <reubenjub@gmail.com>
2025-06-10 14:52:20 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -2,22 +2,17 @@
     <div class="section-container">
         <div class="about-content">
             <div class="about-text">
-                    <p>
                         Reuben is a writer, maker, and photographer interested in design, 
-                        culture, and the spatial impacts of the climate crisis. He is based in East London.
-                    </p> <p>
+                        culture, and the spatial impacts of the climate crisis. He is based in East London.<br><br>
                         Reuben is digital editor at The Architectural Review, where he writes on 
                         architecture and politics. He also works on digital strategy and editorial 
-                        projects at the AR and edits On Site, a newsletter.
-                    </p> <p>
+                        projects at the AR and edits On Site, a newsletter.<br><br>
                         He graduated from the University of Cambridge in 2023, where he was 
                         president of ARCSOC, the university architecture society, and wrote for 
                         numerous student newspapers and magazines. He was also assistant 
-                        producer at New York-based global design platform The World Around.
-                    </p> <p>
+                        producer at New York-based global design platform The World Around.<br><br>
                         His first gig, at two months old, was a starring role in the music video 
                         for "You Were Right" by Badly Drawn Boy.
-                    </p>
             </div>
              <div class="about-image">
                 <img src="<?php echo esc_url(home_url('wp-content/uploads/2025/06/Reuben-J.-Brown-journalist-headshot-monochrome-copy.webp')); ?>" alt="Reuben J. Brown" />
