754ee49b5adca3d3a887d8501aca955b50f80895
reubenj-brown <reubenjub@gmail.com>
2025-07-01 12:09:30 +0100

    Create strategy-section.php

---
+++ templates/strategy-section.php
@@ -0,0 +1,31 @@
+<section class="content-section strategy-section" id="strategy">
+    <div class="section-container">
+        <div class="strategy-content">
+            <div class="strategy-grid">
+                <div class="strategy-item">
+                    <a href="#" class="strategy-link">
+                        <h2 class="strategy-title">Audience<br>Strategy</h2>
+                    </a>
+                </div>
+                
+                <div class="strategy-item">
+                    <a href="#" class="strategy-link">
+                        <h2 class="strategy-title">Artificial<br>Intelligence</h2>
+                    </a>
+                </div>
+                
+                <div class="strategy-item">
+                    <a href="#" class="strategy-link">
+                        <h2 class="strategy-title">Web Design</h2>
+                    </a>
+                </div>
+                
+                <div class="strategy-item">
+                    <a href="#" class="strategy-link">
+                        <h2 class="strategy-title">Leadership</h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
