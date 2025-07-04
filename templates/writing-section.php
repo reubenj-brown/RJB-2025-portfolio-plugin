c902500ab3483d95781bd1ea065ea5fbfb3e6309
Reuben J. Brown <reubenjub@gmail.com>
2025-07-04 22:51:05 +0100

    Update writing-section.php

--- templates/writing-section.php
+++ templates/writing-section.php
@@ -65,15 +65,15 @@
                 <div class="story-links">
                     <a href="#" class="story-link">
                         <p class="link-text">Learning from the 'additive architecture' of Jørn Utzon, a community centre for Syrian refugees in Turkey provides social infrastructure in a fortified shell</p>
-                        <p class="link-date">August 2024</p>
+                        <p class="link-date">August 2024 &nbsp&nbsp→</p>
                     </a>
                     <a href="#" class="story-link">
                         <p class="link-text">Indian rising stars A Threshold build a critical bridge from private commissions to public projects</p>
-                        <p class="link-date">November 2025</p>
+                        <p class="link-date">November 2025 &nbsp&nbsp→</p>
                     </a>
                     <a href="#" class="story-link">
                         <p class="link-text">Protest architecture: timber pallets, climbing ropes and a bottle of glitter</p>
-                        <p class="link-date">November 2025</p>
+                        <p class="link-date">November 2025 &nbsp&nbsp→</p>
                     </a>
                 </div>
             </div>
