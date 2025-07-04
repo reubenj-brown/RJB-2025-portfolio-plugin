39df93e1b3561f1732135969c3a276b59b47b613
Reuben J. Brown <reubenjub@gmail.com>
2025-07-04 23:57:55 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -2,17 +2,11 @@
     <div class="section-container">
         <div class="about-content">
             <div class="about-text">
-                <p>Reuben is a writer, maker, and photographer interested in design, 
-                culture, and the spatial impacts of the climate crisis. He is based in East London.</p>
-                <p>Reuben is digital editor at The Architectural Review, where he writes on 
-                architecture and politics. He also works on digital strategy and editorial 
-                projects at the AR and edits On Site, a newsletter.</p>
-                <p>He graduated from the University of Cambridge in 2023, where he was 
-                president of ARCSOC, the university architecture society, and wrote for 
-                numerous student newspapers and magazines. He was also assistant 
-                producer at New York-based global design platform The World Around.</p>
-                <p>His first gig, at two months old, was a starring role in the music video 
-                for "You Were Right" by Badly Drawn Boy.</p>
+                <p>Hi – I’m Reuben, a multimedia journalist with a background in design and architecture. I’m interested in very big stories and the human lives within them. The undocumented migrants propping up continental food systems; the complex personal costs from the big bets we need to combat the climate crisis; the local planning board decisions that gum up national housing targets; the individuals leading innovation and pushing forwards change in big business and organisations.</p>
+                <p>This year, I’ll be pursuing stories like this as a Steele Fellow in Investigative Business Journalism at the Reynolds Centre, part of the Walter Cronkite School of Journalism at Arizona State University. The fellowship is a real privilege: a tuition-free opportunity to take Cronkite’s Masters of Journalism and Mass Communication, while earning a stipend reporting out stories for the Reynolds Centre under the direction of Jeffrey Timmermans.</p>
+                <p>I work best in tight-knit teams with outsized impacts. Tough, go-getting teams with strong missions and intense collaboration. That was my experience at <i>The Architectural Review</i>, where I was Digital Editor, devising and delivering on the magazine’s audience strategy, creating and editing a new newsletter and leading the brand’s social media presence to new heights. There was also a lot of writing – on the architecture triennial in Sharjah, Syrian refugee centres Turkey, the architecture of protest in Central Europe, and some great architects and buildings.</p>
+                <p>I came to <i>The Architectural Review</i> as a graduate from Cambridge university. There, I was president of (likely) the world’s only student architecture society to self-fund, self-design and self-deliver its university’s annual exhibition: displaying 250 students’ work in central London. We also developed a new website, made top billing of every section of the student newspaper, and filled the pandemic-shaped hole in ARCSOC’s finances. Before this, I was a Production Assistant at The World Around, a New York-based global design platform, where I helped to launch the Young Climate Prize, secured $10,000/mo in Google Ads Funding, led work to repositioning the brand’s YouTube channel, and met a global group of geniuses.</p> 
+                <p>I’ve had many weird jobs, but my first gig – at two months old – was a starring role in the music video for "You Were Right" by Badly Drawn Boy.</p>
             </div>
              <div class="about-image">
                 <img src="<?php echo esc_url(home_url('wp-content/uploads/2025/06/Reuben-J.-Brown-journalist-headshot-monochrome-copy.webp')); ?>" alt="Reuben J. Brown" />
