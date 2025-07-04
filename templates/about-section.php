840df5fd665ca88b86021e812948b3575aefb3d3
Reuben J. Brown <reubenjub@gmail.com>
2025-07-05 00:18:26 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -3,7 +3,7 @@
         <div class="about-content">
             <div class="about-text">
                 <h3>Hi – I’m Reuben, a multimedia journalist with a background in design. I’m interested in very big problems and the human lives within them.</h3><br>
-                <p>TThis year, I’ll be pursuing stories like this as a Steele Fellow in Investigative Business Journalism at the Walter Cronkite School of Journalism, Arizona State University.</p>
+                <p>This year, I’ll be pursuing stories like this as a Steele Fellow in Investigative Business Journalism at the Walter Cronkite School of Journalism, Arizona State University.</p>
                 <p>Before this, I was Digital Editor at The Architectural Review, working on audience strategy across platforms. I wrote on architecture and politics at the AR, at the architecture triennial in Sharjah,  or on Syrian refugee centres Turkey, or the architecture of protest in Central Europe. I graduated from the University of Cambridge, where I was president of (likely) the world’s only student architecture society to self-fund, self-design and self-deliver its university’s annual exhibition: displaying 250 students’ work in central London. I was also Production Assistant at The World Around, a New York-based global design platform, where I helped to launch the Young Climate Prize, and worked on TV shoots for BBC Maestro.</p>
                 <p>My first gig – at two months old – was a starring role in the music video for "You Were Right" by Badly Drawn Boy.</p>
             </div>
