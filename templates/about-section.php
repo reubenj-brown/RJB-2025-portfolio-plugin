4cc2afa270286afff037443265cbe8706e33447c
Reuben J. Brown <reubenjub@gmail.com>
2025-07-12 15:56:51 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -2,7 +2,7 @@
     <div class="section-container">
         <div class="about-content">
             <div class="about-text">
-                <h2 class="serif-font-scaled">I’m Reuben, a multimedia journalist with a background in design. I’m interested in big systems and the human lives within them.</h2>
+                <h2 class="serif-font-scaled">I’m Reuben, a multimedia journalist with a background in design. I’m interested in big systems and the human lives within them.</h2><br>
                 <p>Think the migrant workers propping up continental food systems; the complex personal costs of the huge projects we need to combat the climate crisis; the local planning board decisions that gum up national housing targets; the individual innovators pushing change in big businesses and organisations.</p>
                 <p>This year, I’ll be pursuing stories like this as a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow in Investigative Business Journalism</a> at the Walter Cronkite School of Journalism, Arizona State University.</p>
                 <p>Before this, I was Digital Editor at <i>The Architectural Review</i>, where I wrote on architecture and politics and worked on cross-platform audience strategy. I graduated from the University of Cambridge, where I was president of (likely) the world’s only student architecture society to self-fund, self-design and self-deliver its university’s annual exhibition. I was also Production Assistant at The World Around, a New York-based global design platform, where I helped to launch the Young Climate Prize, and worked on TV shoots for the BBC</p>
