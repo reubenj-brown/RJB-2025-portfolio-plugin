4115386901e2c1c99a1a10394956f52db5808973
Reuben J. Brown <reubenjub@gmail.com>
2025-07-12 22:10:29 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -7,8 +7,8 @@
             <div class="about-text">
                 <p>Think the migrant workers propping up continental food systems; the complex personal costs of the huge projects we need to combat the climate crisis; the local planning board decisions that gum up national housing targets; the individual innovators pushing change in big businesses and organisations.</p>
                 <p>This year, I’m pursuing these stories as a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow in Investigative Business Journalism</a> at the Walter Cronkite School of Journalism, Arizona State University.</p>
-                <p>Before this, I was Digital Editor at <i>The Architectural Review</i>, where I wrote on architecture and politics and worked on devising and delivering cross-platform audience strategy.</p>
-                <p>I graduated from the University of Cambridge, where I was president of the student architecture society, self-funding, designing, and delivering our annual exhibition in London – among much else. I was Production Assistant at The World Around, a New York-based global design platform where I helped to launch the Young Climate Prize, and worked on TV shoots for BBC Maestro.</p>
+                <p>Before this, I was Digital Editor at <i>The Architectural Review</i>, where I wrote on architecture and politics and worked on cross-platform audience strategy.</p>
+                <p>I graduated from the University of Cambridge, where I was president of ARCSOC, likely the only student architecture society to self-fund, design, and deliver its university’s annual exhibition – among much else. I was Production Assistant at The World Around, a New York-based global design platform where I helped to launch the Young Climate Prize, and worked on TV shoots for BBC Maestro.</p>
                 <p>My first gig – at two months old – was a starring role in the music video for “You Were Right” by Badly Drawn Boy.</p>
             </div>
         </div>
