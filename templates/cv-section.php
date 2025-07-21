f35423fc99cb186581c326bccb196b517e8dfad8
Reuben J. Brown <reubenjub@gmail.com>
2025-07-21 19:19:28 +0100

    Update cv-section.php

--- templates/cv-section.php
+++ templates/cv-section.php
@@ -52,7 +52,7 @@
                         <p class="cv-role">Web Development</p>
                         <p class="cv-dates">GitHub, VS Code, Claude Code, HTML, CSS, JS</p>
                             <br>
-                        <p class="cv-dates">Full UK driving license</p>
+                        <p class="cv-dates">Full U.K. driving license</p>
                     </div>
             
             <div class="cv-column education-column">
