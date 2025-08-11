2931774048f9d0b80c53e4f9dee492eb93d042fe
Reuben J. Brown <reubenjub@gmail.com>
2025-08-11 13:23:53 +0100

    Update about-section.php

--- templates/about-section.php
+++ templates/about-section.php
@@ -5,10 +5,10 @@
                 <p class="large-intro-text">I’m Reuben, a multimedia journalist with a background in design. I’m interested in very big systems, the people shaping them, and the people they shape.</p>
             </div>
             <div class="about-text">
-                <p>Think the migrant workers propping up continental food systems; the local tradeoffs of nation-spanning climate projects; the picayune planning edicts that gum up city housing targets; the individual innovators pushing change in big businesses and organisations.</p>
-                <p>This year, I’m pursuing these stories as a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow in Investigative Business Journalism</a> at the Walter Cronkite School of Journalism, Arizona State University.</p>
-                <p>Before this, I was Digital Editor at <i>The Architectural Review</i>, where I wrote on architecture and politics and worked on cross-platform audience strategy. I graduated from the University of Cambridge, where I was president of the architecture society, delivering an exhibition of 250 students’ work in London, and much, much more. I was Production Assistant at The World Around, a New York-based design platform where I helped to launch the Young Climate Prize, and worked on TV shoots for BBC Maestro.</p>
-                <p>My first gig – at two months old – was a starring role in the music video for “You Were Right” by Badly Drawn Boy.</p>
+                <p class="about-text">Think the migrant workers propping up continental food systems; the local tradeoffs of nation-spanning climate projects; the picayune planning edicts that gum up city housing targets; the individual innovators pushing change in big businesses and organisations.</p>
+                <p class="about-text">This year, I’m pursuing these stories as a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow in Investigative Business Journalism</a> at the Walter Cronkite School of Journalism, Arizona State University.</p>
+                <p class="about-text">Before this, I was Digital Editor at <i>The Architectural Review</i>, where I wrote on architecture and politics and worked on cross-platform audience strategy. I graduated from the University of Cambridge, where I was president of the architecture society, delivering an exhibition of 250 students’ work in London, and much, much more. I was Production Assistant at The World Around, a New York-based design platform where I helped to launch the Young Climate Prize, and worked on TV shoots for BBC Maestro.</p>
+                <p class="about-text">My first gig – at two months old – was a starring role in the music video for “You Were Right” by Badly Drawn Boy.</p>
             </div>
         </div>
     </div>
