4e1935edcfc943df11f09895a358b6d6e2624b0b
Reuben J. Brown <reubenjub@gmail.com>
2025-09-24 22:19:40 -0700

    Update cv-section.php

--- templates/cv-section.php
+++ templates/cv-section.php
@@ -87,10 +87,7 @@
         
         </div>
     </div>
-</section>
-
-
         <div class="strategy-intro">
-                <h3 class="serif-font-scaled">My first gig – at two months old – was a starring role in the for “You Were Right” by Badly Drawn Boy. Watch that <a href="https://youtu.be/K1BNOzDnOLI?t=202">here</a>, or contact me for actually serious things below:</h3>
+            <h3 class="serif-font-scaled">My first gig – at two months old – was a starring role in the for “You Were Right” by Badly Drawn Boy. Watch that <a href="https://youtu.be/K1BNOzDnOLI?t=202">here</a>, or contact me for actually serious things below:</h3>
         </div> 
-
+</section>
