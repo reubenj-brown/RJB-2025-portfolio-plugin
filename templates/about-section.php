67a675d6e19e9592ee988f22e571cb35871ec203
Reuben J. Brown <reubenjub@gmail.com>
2025-10-23 12:05:17 -0700

    Update about section introduction

--- templates/about-section.php
+++ templates/about-section.php
@@ -2,7 +2,7 @@
     <div class="section-container">
         <div class="about-content">
             <div class="about-intro">
-                <p class="large-intro-text">I’m Reuben, a multimedia journalist with a background in design. I’m interested in very big systems, the people shaping them, and the people they shape</p>
+                <p class="large-intro-text">I’m Reuben, a multimedia journalist with a background in design. My work spans narrative reporting, photography, audio, and video. I’m interested in big systems, the people shaping them, and the people they shape</p>
             </div>
         </div>
     </div>
