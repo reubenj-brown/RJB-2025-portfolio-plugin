57b2ddb3aa0573ae2537bcf74308c4274a733b07
Reuben J. Brown <reubenjub@gmail.com>
2026-01-31 18:13:58 -0700

    Fix typographical errors in cronkite.php

--- templates/cronkite.php
+++ templates/cronkite.php
@@ -5,7 +5,7 @@
             <span class="display-headline">Arizona</span>
         </div>
         <div class="strategy-intro-body">
-            <h3>I'm currently based in Phoenix, pursuing an MA in Investigative Journalism at the Walter Cronkite School. I'm also a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow</a> at Cronkite's Reynolds Center for Business Journalism</h3>
+            <h3>I’m currently based in Phoenix, pursuing an MA in Investigative Journalism at the Walter Cronkite School. I’m also a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow</a> at Cronkite’s Reynolds Center for Business Journalism</h3>
         </div>
     </div>
     <section class="content-section">
