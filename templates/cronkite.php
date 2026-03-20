1719bab6a5cd5ea4c2f0c7ca153a4db6110c5945
Reuben J. Brown <reubenjub@gmail.com>
2026-03-20 11:15:09 -0700

    Remove 'at the Reynolds Center' from bio

--- templates/cronkite.php
+++ templates/cronkite.php
@@ -5,7 +5,7 @@
             <span class="display-headline">Arizona</span>
         </div>
         <div class="strategy-intro-body">
-            <h3>I’m currently pursuing an M.A. in Investigative Journalism at the Walter Cronkite School, where I am also a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow</a> in Investigative Business Journalism at the Reynolds Center</h3>
+            <h3>I’m currently pursuing an M.A. in Investigative Journalism at the Walter Cronkite School, where I am also a <a href="https://cronkite.asu.edu/specializations/business-journalism-fellowship-graduate-fellowship/">Steele Fellow</a> in Investigative Business Journalism</h3>
         </div>
     </div>
     <section class="content-section">
